26a3b4842632f708838d513bdf013bbb2ab970d4
gaob <nightfir@mail.com>
2015-05-11 11:18:26 +0800

    Add basic unit test method. Not complete.

---
+++ UnitTestProject/UnitTest1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using HorseMobileService;
+using CustomAPIAMobileService.Controllers;
+using Microsoft.WindowsAzure.Mobile.Service;
+using Moq;
+using System.Net;
+using System.Threading.Tasks;
+using System.Web.Http.Controllers;
+using HorseMobileService.Controllers;
+
+namespace UnitTestProject
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        /// <summary>
+        /// Test method to test meController.
+        /// Not completed yet.
+        /// </summary>
+        /// <returns></returns>
+        [TestMethod]
+        public async Task TestMethod1()
+        {
+            var controller = new meController();
+
+            var mock = new Mock<HttpControllerContext>();
+            controller.Request = new System.Net.Http.HttpRequestMessage();
+            controller.Configuration = new System.Web.Http.HttpConfiguration();
+            mock.SetupGet(x => x.RequestContext.Principal.Identity.Name).Returns("SOMEUSER");
+            mock.SetupGet(x => x.RequestContext.Principal.Identity.IsAuthenticated).Returns(true);
+            controller.ControllerContext = mock.Object;
+
+            var me = await controller.Get();
+
+            Assert.AreEqual(me.StatusCode, HttpStatusCode.OK);
+        }
+    }
+}
